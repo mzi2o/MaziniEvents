e9f862e4a9fd319f182087d5fa7977a757075935
Marwa Mazini <marwa6mzi@gmail.com>
2026-07-15 16:57:17 +0100

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
+  base: "/MaziniEvents/",
+
   plugins: [react(), tailwindcss()],
 
   resolve: {
